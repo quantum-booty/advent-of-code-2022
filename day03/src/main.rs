910615b6908ff1912ee6e24753cd8f0cef261ba9
Henry Wang <henrywang97@gmail.com>
2022-12-03 16:16:41 +0000

    clean up day 3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -11,8 +11,9 @@ jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw";
-    let input = include_str!("inputs.txt");
+CrZsJsPPZsGzwwsLwLmpwMDw"
+        .as_bytes();
+    let input = include_bytes!("inputs.txt");
 
     println!("{}", solution_a(test_input));
     println!("{}", solution_a(input));
@@ -20,32 +21,26 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
     println!("{}", solution_b(input));
 }
 
-fn solution_b(input: &str) -> u32 {
+fn solution_b(input: &[u8]) -> u32 {
     input
-        .lines()
+        .split(|n| *n == b'\n')
         .array_chunks::<3>()
         .map(|lines| {
-            let mut iter = lines
+            let intersection = lines
                 .iter()
-                .map(|line| HashSet::<_>::from_iter(line.as_bytes()));
-            let intersection = iter
-                .next()
-                .map(|set| {
-                    iter.fold(set, |set1, set2| {
-                        set1.intersection(&set2).copied().collect()
-                    })
-                })
+                .map(|line| HashSet::<_>::from_iter(*line))
+                .reduce(|set1, set2| set1.intersection(&set2).copied().collect())
                 .unwrap();
             char_to_priority(**intersection.iter().next().unwrap())
         })
         .sum()
 }
 
-fn solution_a(input: &str) -> u32 {
+fn solution_a(input: &[u8]) -> u32 {
     input
-        .lines()
-        .map(|line| {
-            let letters = line.as_bytes();
+        .split(|n| *n == b'\n')
+        .filter(|letters| !letters.is_empty())
+        .map(|letters| {
             let (left, right) = letters.split_at(letters.len() / 2);
             let left = HashSet::<_>::from_iter(left);
             let right = HashSet::<_>::from_iter(right);
