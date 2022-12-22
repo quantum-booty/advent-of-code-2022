51a471a0cecbc88511734b67e95a4eec0ca3a5c7
Henry Wang <henrywang97@gmail.com>
2022-12-22 22:16:08 +0000

    add day21

--- day21/src/lib.rs
+++ day21/src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(int_roundings)]
 use std::collections::HashMap;
 
 use nom::{
@@ -13,7 +14,7 @@ use nom::{
 use Monkey::*;
 use Operation::*;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Operation {
     Add,
     Sub,
@@ -28,6 +29,28 @@ impl Operation {
             Sub => left - right,
             Div => left / right,
             Mul => left * right,
+        }
+    }
+
+    fn inverse_calculate(&self, result: i64, left: Option<i64>, right: Option<i64>) -> i64 {
+        // left = x Operation right
+        match self {
+            Add => result - left.or(right).unwrap(),
+            Sub => match (left, right) {
+                // result = x - right
+                (None, Some(right)) => result + right,
+                // result = left - x
+                (Some(left), None) => left - result,
+                _ => unreachable!(),
+            },
+            Div => match (left, right) {
+                // result = x / right
+                (None, Some(right)) => result * right,
+                // result = right / x
+                (Some(right), None) => right / result,
+                _ => unreachable!(),
+            },
+            Mul => result / left.or(right).unwrap(),
         }
     }
 }
@@ -50,6 +73,77 @@ fn calculate(monkeys: &HashMap<&str, Monkey>, monkey: &str) -> i64 {
             operation.calculate(calculate(monkeys, left), calculate(monkeys, right))
         }
         Number(number) => *number,
+    }
+}
+
+#[derive(Debug)]
+enum MonkeyRec {
+    Job(Operation, Box<MonkeyRec>, Box<MonkeyRec>),
+    Number(i64),
+    Human,
+}
+
+fn calculate_rec(monkeys: &HashMap<&str, Monkey>, monkey: &str) -> MonkeyRec {
+    if monkey == "humn" {
+        return MonkeyRec::Human;
+    }
+    match &monkeys[monkey] {
+        Job(operation, left, right) => {
+            let left_result = calculate_rec(monkeys, left);
+            let right_result = calculate_rec(monkeys, right);
+            match (&left_result, &right_result) {
+                (MonkeyRec::Number(left_result), MonkeyRec::Number(right_result)) => {
+                    MonkeyRec::Number(operation.calculate(*left_result, *right_result))
+                }
+                _ => MonkeyRec::Job(*operation, Box::new(left_result), Box::new(right_result)),
+            }
+        }
+        Number(number) => MonkeyRec::Number(*number),
+    }
+}
+
+pub fn solution_b(input: &str) -> i64 {
+    let (_, monkeys) = parse(input).unwrap();
+    let monkeys = HashMap::<&str, Monkey>::from_iter(monkeys);
+
+    match &monkeys["root"] {
+        Job(_, left, right) => {
+            let left = calculate_rec(&monkeys, left);
+            let right = calculate_rec(&monkeys, right);
+            match (&left, &right) {
+                (MonkeyRec::Number(lhs), MonkeyRec::Job(..)) => solve_equality(*lhs, right),
+                (MonkeyRec::Job(..), MonkeyRec::Number(lhs)) => solve_equality(*lhs, left),
+                _ => unreachable!(),
+            }
+        }
+        _ => unreachable!(),
+    }
+}
+
+fn solve_equality(result: i64, rhs: MonkeyRec) -> i64 {
+    match rhs {
+        MonkeyRec::Job(operation, left, right) => {
+            let left = *left;
+            let right = *right;
+            match (&left, &right) {
+                (MonkeyRec::Number(left), MonkeyRec::Job(..)) => solve_equality(
+                    operation.inverse_calculate(result, Some(*left), None),
+                    right,
+                ),
+                (MonkeyRec::Job(..), MonkeyRec::Number(right)) => solve_equality(
+                    operation.inverse_calculate(result, None, Some(*right)),
+                    left,
+                ),
+                (MonkeyRec::Number(left), MonkeyRec::Human) => {
+                    operation.inverse_calculate(result, Some(*left), None)
+                }
+                (MonkeyRec::Human, MonkeyRec::Number(right)) => {
+                    operation.inverse_calculate(result, None, Some(*right))
+                }
+                _ => unreachable!(),
+            }
+        }
+        _ => unreachable!(),
     }
 }
 
@@ -111,5 +205,7 @@ hmdt: 32";
     fn it_works() {
         assert_eq!(solution_a(TEST_INPUT), 152);
         println!("{}", solution_a(INPUT));
+        assert_eq!(solution_b(TEST_INPUT), 301);
+        println!("{}", solution_b(INPUT));
     }
 }
